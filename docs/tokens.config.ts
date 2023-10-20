7dae1c75316c94d99ea1a17a738de2f4c19f0fbd
Daniel Dahlström <danadalis@users.noreply.github.com>
2023-10-20 23:44:32 +0200

    Change primary color

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,4 +1,18 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    primary: {
+      '50': '#fcffe6',
+      '100': '#f8ffcc',
+      '200': '#f1ff99',
+      '300': '#ebff66',
+      '400': '#e4ff33',
+      '500': '#ddff00',
+      '600': '#b1cc00',
+      '700': '#859900',
+      '800': '#586600',
+      '900': '#2c3300'
+    }
+  }
 })
